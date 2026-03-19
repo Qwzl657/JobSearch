da4321d569916a1bd1978b642cd2fc68f44be866
Alibek <sultanovalibek2020@gmail.com>
2026-03-19 16:45:56 +0600

    add service layer

--- src/main/java/kg/attractor/lesson_49/service/UserService.java
+++ src/main/java/kg/attractor/lesson_49/service/UserService.java
@@ -1,7 +1,13 @@
 package kg.attractor.lesson_49.service;
 
 import kg.attractor.lesson_49.dto.UserDto;
+import kg.attractor.lesson_49.model.User;
+
+import java.util.List;
 
 public interface UserService {
-    UserDto getSampleUser();
+
+    UserDto getSampleUser(); // старый метод НЕ трогаем
+
+    List<User> getAllUsers(); // новый
 }
--- src/main/java/kg/attractor/lesson_49/service/impl/UserServiceImpl.java
+++ src/main/java/kg/attractor/lesson_49/service/impl/UserServiceImpl.java
@@ -1,19 +1,31 @@
 package kg.attractor.lesson_49.service.impl;
 
+import kg.attractor.lesson_49.dao.UserDao;
 import kg.attractor.lesson_49.dto.UserDto;
+import kg.attractor.lesson_49.model.User;
 import kg.attractor.lesson_49.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserDao userDao;
+
     @Override
-    public UserDto getSampleUser(){
-        UserDto user = new UserDto();
-        user.setUsername("Alex");
-        user.setEmail("qwe@qwe.qwe");
-        user.setPassword("qwerty");
-        return user;
+    public UserDto getSampleUser() {
+        return UserDto.builder()
+                .username("test")
+                .email("test@mail.com")
+                .password("123")
+                .build();
     }
 
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.findAll();
+    }
 }
